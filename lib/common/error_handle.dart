ab0a10e9f3869d97c35f21f210fd495490a04315
Uday-kiran9147 <udaykiran9147@gmail.com>
2023-02-18 12:11:23 +0530

    usertest1

---
+++ lib/common/error_handle.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:amazonn/constants/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void httpErrorHandle(
+    {required http.Response response,
+    required BuildContext context,
+    required VoidCallback onSuccess}) {
+  switch (response.statusCode) {
+    case 200:
+      onSuccess();
+      break;
+    case 400:
+      ShowSnackBar(context, jsonDecode(response.body)['msg']);
+      break;
+    case 500:
+      ShowSnackBar(context, jsonDecode(response.body)['errror']);
+      break;
+    default:
+      ShowSnackBar(context, response.body);
+  }
+}
---
+++ lib/features/Auth/Screens/Services/auth_service.dart
@@ -0,0 +1,44 @@
+import 'package:amazonn/Models/user.dart';
+import 'package:amazonn/common/error_handle.dart';
+import 'package:amazonn/constants/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../constants/global_vars.dart';
+
+class AuthService {
+  //signup
+  void signupUser(
+      {required BuildContext context,
+      required email,
+      required password,
+      required name}) async {
+    try {
+      User user = User(
+          id: "",
+          name: "",
+          email: "",
+          password: "",
+          address: "",
+          type: "",
+          token: "");
+
+      http.Response res = await http.post(
+        Uri.parse("$uri/api/signup"),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'appllication/json; charset=UTF-8',
+        },
+      );
+      print(res.statusCode);
+      httpErrorHandle(
+          response: res,
+          context: context,
+          onSuccess: () {
+            ShowSnackBar(context, "Account has been created successfully");
+          });
+    } catch (e) {
+      ShowSnackBar(context, e.toString());
+    }
+  }
+}
